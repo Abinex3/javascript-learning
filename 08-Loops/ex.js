3be6a023e032d5a1914ad2e5fe1012a56d7cbe71
Abinex3 <abineshmuniyasaamy@gmail.com>
2026-08-04 21:16:26 +0530

    loops started

---
+++ 08-Loops/ex.js
@@ -0,0 +1,16 @@
+// let i = 0;
+// while(i<5){
+//     console.log(i++);
+// }
+
+// Task 1 : Print numbers from 1 to 10.
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i++);   
+// }
+
+// Task 2 : Print numbers from 10 down to 1.
+let j = 10;
+while (j >= 1) {
+    console.log(j--);
+}
---
+++ 08-Loops/notes.js
@@ -0,0 +1,22 @@
+// Loops: while and for
+// We often need to repeat actions.
+// For example, outputting goods from a list one after another or just running the same code for each number from 1 to 10.
+// Loops are a way to repeat the same code multiple times.
+
+// The “while” loop
+// The while loop has the following syntax:
+
+// while (condition) {
+//   // code
+//   // so-called "loop body"
+// }
+// While the condition is truthy, the code from the loop body is executed.
+
+// For instance, the loop below outputs i while i < 3:
+
+// let i = 0;
+// while (i < 3) { // shows 0, then 1, then 2
+//   alert( i );
+//   i++;
+// }
+
